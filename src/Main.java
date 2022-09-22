a1ae0114b9f8394d8c1ad6ae1bc04030d2412fd2
mojzesz2113 <miloszgit2115@gmail.com>
2022-09-22 16:09:35 +0200

    Zadania z mobi

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -41,5 +42,31 @@ public class Main {
             }
         }
         System.out.println(liczbyPierwsze);
+
+        //przygotuj listę z 10 liczbami wylosowanymi z zakresu od 30 do 60 bez powtórzeń
+        HashSet<Integer> losowanie = new HashSet<>();
+        while(losowanie.size()<10){
+            losowanie.add((int)(Math.random()*30+31));
+        }
+        System.out.println(losowanie);
+        //tablica 10 liczb wypełniona pierwiastkami
+        double pierwiastki [] = new double[10];
+        for(int i=0;i<10;i++){
+            pierwiastki[i] = Math.pow(i, 0.5);
+        }
+        for(double wartosc: pierwiastki){
+            System.out.print(wartosc+", ");
+        }
+        //liczby Fibbonacciego
+        System.out.println("Liczby Fibbonacciego");
+        ArrayList<Integer> fibo = new ArrayList<>();
+        fibo.add(0);
+        fibo.add(1);
+        int indeks = 2;
+        while(fibo.size()<40){
+            fibo.add(fibo.get(indeks-2)+fibo.get(indeks-1));
+            indeks++;
+        }
+        System.out.println(fibo);
     }
 }
